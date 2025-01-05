7f21f60218ac2a97ef32f010ea1e1978a484c21a
phamxuantoan1996 <phamxuantoandc@gmail.com>
2025-01-05 14:15:12 +0700

    modify detect_object.py 5_1_2025

--- detect_object.py
+++ detect_object.py
@@ -3,49 +3,26 @@ import numpy as np
 import open3d as o3d
 import cv2
 from ultralytics import YOLO
-import math
-from PIL import Image
-import torch
-
-
-
-def check_distance_human(roi,rgb_image,depth_frame,color_intrin) -> bool:
-    x1, y1, x2, y2 = roi
-    centerx = x1 + round((x2-x1)/2)
-    centery = y1 + round((y2-y1)/2)
-    cv2.circle(rgb_image,(centerx,centery),2,(0,0,255),2)
-
-    depth = depth_frame.get_distance(centerx, centery)
-    dx ,dy, dz = rs.rs2_deproject_pixel_to_point(color_intrin, [centerx,centery], depth)
-    distance = math.sqrt(((dx)**2) + ((dy)**2) + ((dz)**2))
-    print('distance : ',distance)
-    if distance > 1.0:
-        return True
-    
-    return False
 
 def camera_config():
     # Create a pipeline
     pipeline = rs.pipeline()
-
     # Create a config and configure the pipeline to stream
     #  different resolutions of color and depth streams
     config = rs.config()
-
     # Get device product line for setting a supporting resolution
-    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-    pipeline_profile = config.resolve(pipeline_wrapper)
-    device = pipeline_profile.get_device()
+    # pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    # pipeline_profile = config.resolve(pipeline_wrapper)
+    # device = pipeline_profile.get_device()
     #######################################################################
-
-    found_rgb = False
-    for s in device.sensors:
-        if s.get_info(rs.camera_info.name) == 'RGB Camera':
-            found_rgb = True
-            break
-    if not found_rgb:
-        print("The demo requires Depth camera with Color sensor")
-        exit(0)
+    # found_rgb = False
+    # for s in device.sensors:
+    #     if s.get_info(rs.camera_info.name) == 'RGB Camera':
+    #         found_rgb = True
+    #         break
+    # if not found_rgb:
+    #     print("The demo requires Depth camera with Color sensor")
+    #     exit(0)
 
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
@@ -54,9 +31,9 @@ def camera_config():
     profile = pipeline.start(config)
 
     # Getting the depth sensor's depth scale (see rs-align example for explanation)
-    depth_sensor = profile.get_device().first_depth_sensor()
-    depth_scale = depth_sensor.get_depth_scale()
-    print("Depth Scale is: " , depth_scale)
+    # depth_sensor = profile.get_device().first_depth_sensor()
+    # depth_scale = depth_sensor.get_depth_scale()
+    # print("Depth Scale is: " , depth_scale)
 
     # Create an align object
     # rs.align allows us to perform alignment of depth frames to others frames
@@ -66,7 +43,7 @@ def camera_config():
 
     return align,pipeline
 
-def check_human(align,pipeline):
+def check_human(align,pipeline,thres = 1.0) -> bool:
     try:
         while True:
             # Get frameset of color and depth
@@ -75,66 +52,55 @@ def check_human(align,pipeline):
 
             # Align the depth frame to color frame
             aligned_frames = align.process(frames)
-
             # Get aligned frames
-            
             aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
             color_frame = aligned_frames.get_color_frame()
-            
             # Validate that both frames are valid
             if not aligned_depth_frame or not color_frame:
                 continue
 
-            depth_image = np.asanyarray(aligned_depth_frame.get_data())
+            # depth_image = np.asanyarray(aligned_depth_frame.get_data())
             color_image = np.asanyarray(color_frame.get_data())
-            color_intrin = color_frame.profile.as_video_stream_profile().intrinsics
+            # color_intrin = color_frame.profile.as_video_stream_profile().intrinsics
                 
-
-
-            # model = YOLO("yolo11n-seg.pt")
-            # results = model.track(color_image)
-            # for result in results:
-            #     boxes = result.boxes
-            #     masks = result.masks
-            #     ori_img = result.orig_img
-            #     for box in boxes:
-            #         class_id = int(box.cls[0])
-            #         class_name = model.names[class_id]
-            #         x1, y1, x2, y2 = box.xyxy[0]
-            #         confidence = box.conf[0]
-            #         label = f"{class_name} {confidence:.2f}"
-            #         if class_name == 'person':
-            #             mask = masks[class_id].data[0].numpy()
-            #             print(class_id)
-            #             print(class_name)
-            #             cv2.rectangle(color_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-            #             cv2.putText(color_image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-            #             cv2.imshow('mask',mask)
+            model = YOLO("yolo11n-seg.pt")
+            results = model.predict(source=color_image, save=False, classes=[0])
+            arr_np = np.zeros(shape=(480,640),dtype=np.uint8)
+            img_person = arr_np.reshape(480,640)
+            for result in results:
+                masks = result.masks
+                if masks != None:
+                    for mask in masks:
+                        mask_person = (mask.data[0].numpy())*255
+                        person = mask_person.astype(np.uint8)
+                        tmp = cv2.bitwise_or(img_person,person)
+                        img_person = tmp
+            for result in results:
+                boxes = result.boxes
+                for box in boxes:
+                    confidence = box.conf[0]
+                    class_id = int(box.cls[0])
+                    class_name = model.names[class_id]
+                    x1, y1, x2, y2 = box.xyxy[0]
+                    label = f"{class_name} {confidence:.2f}"
+                    if class_name == "person":          
+                        point_cloud = []
+                        for x in range(int(x1),int(x2)):
+                            for y in range(int(y1),int(y2)):
+                                if img_person[y,x] != 0:
+                                    if aligned_depth_frame.get_distance(x,y) > 0:
+                                        point_cloud.append(aligned_depth_frame.get_distance(x,y))
+                        if len(point_cloud) > 0:
+                            dis = min(point_cloud)
+                            dis = round(dis,4)
+                            if dis > thres:
+                                cv2.rectangle(color_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+                            else:
+                                cv2.rectangle(color_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
+                            cv2.putText(color_image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                            cv2.putText(color_image, str(dis), (int(x1), int(y1) + int((int(y2) - int(y1))/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
             
-
-            # model = YOLO("yolo11n-seg.pt")
-            # results = model.track(color_image)
-            # for result in results:
-            #     boxes = result.boxes
-            #     for box in boxes:
-            #         confidence = box.conf[0]
-            #         # Get the class ID and name
-            #         class_id = int(box.cls[0])
-            #         class_name = model.names[class_id]
-            #         x1, y1, x2, y2 = box.xyxy[0]
-            #         label = f"{class_name} {confidence:.2f}"
-            #         # Bước 2 :Lọc nguoi
-            #         if class_name == "person":
-                        
-            #             roi = (int(x1),int(y1),int(x2),int(y2))
-            #             if check_distance_human(roi,color_image,aligned_depth_frame,color_intrin):
-
-            #                 cv2.rectangle(color_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-            #                 cv2.putText(color_image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-            #             else:
-            #                 cv2.rectangle(color_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
-            #                 cv2.putText(color_image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-
+            # cv2.imshow('seg',img_person)
             cv2.imshow('window1',color_image)
             
             key = cv2.waitKey(1)
@@ -147,6 +113,6 @@ def check_human(align,pipeline):
 
 if __name__ == "__main__":
     align,pipeline = camera_config()
-    check_human(align,pipeline)
+    check_human(align,pipeline,thres=0.5)
 
 
